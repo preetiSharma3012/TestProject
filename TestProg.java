17458d685bd20c43ec0b4eea24406112f2de7437
preetiSharma3012 <sharma.preeti3012@gmail.com>
2020-01-19 19:47:58 +0530

    Commit Message

--- TestProg.java
+++ TestProg.java
@@ -3,5 +3,5 @@ public static void main(String[] args)
 {
 for(int i=0;i<=5;i++)
 {
-System.out.println("This is testing program...."+ "see the number"i);
+System.out.println("This is testing program...."+i);
 }}}
